3d443ae85189f7d4e257374fd3ba97fa31844b20
hasan genc <hasangencx@hotmail.com>
2019-11-17 16:39:17 +0300

    Create solution.java

---
+++ arrays/left-rotation/new-year-chaos/solution.java
@@ -0,0 +1,21 @@
+import java.math.*;
+
+public class Solution {
+    static void minimumBribes(int[] q) {
+        int bribes = 0;
+        for(int i = q.length - 1; i >= 0; --i){
+            if((q[i] - (i+1)) > 2){
+                System.out.println("Too chaotic");
+                return; 
+            }
+
+            for (int j = Math.max(0, q[i] - 2); j < i; j++) {
+                if (q[j] > q[i]) {
+                    ++bribes;
+                }
+            }
+        }
+        
+        System.out.println(bribes);
+    }
+}
