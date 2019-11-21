6c912262a7e2387b23b27cf4461d456518b30365
hasan genc <hasangencx@hotmail.com>
2019-11-21 23:32:27 +0300

    Create solution.java

---
+++ arrays/array-manipulation/solution.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+public class Solution {
+
+    // Complete the arrayManipulation function below.
+    static long arrayManipulation(int n, int[][] queries) {
+        int[] arr = new int[n];
+        long max = 0;
+        int temp = 0;
+
+        for (int i = 0; i < queries.length; ++i) {
+            arr[queries[i][0]] += queries[i][2];
+            if (queries[i][1] < n) {
+                arr[queries[i][1]] -= queries[i][2];
+            }
+        }
+
+        for (int j = 0; j < n; ++j) {
+            temp += arr[j];
+            if (temp > max ) {
+                max = temp;
+            }
+        }
+
+        return max;
+    }
+}
