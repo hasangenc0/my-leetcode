21a1ae0bcd8739332e603633fcd314512c4bbf35
hasan genc <hasangencx@hotmail.com>
2019-11-27 15:02:43 +0300

    Optimize

--- hashtables-and-dictionaries/ransom-note/solution.java
+++ hashtables-and-dictionaries/ransom-note/solution.java
@@ -7,21 +7,20 @@ public class Solution {
         Hashtable<String, Integer> m = new Hashtable<String, Integer>(); 
         Hashtable<String, Integer> n = new Hashtable<String, Integer>(); 
 
-        for (int i = 0; i < note.length; ++i) {
-            int count = n.containsKey(note[i]) ? n.get(note[i]) + 1 : 1;
-            n.put(note[i], count);
-        }
-
         for (int i = 0; i < magazine.length; ++i) {
             int count = m.containsKey(magazine[i]) ? m.get(magazine[i]) + 1 : 1;
             m.put(magazine[i], count);
         }
 
-        for (String key : n.keySet()) {
-            if (!(m.containsKey(key) && n.get(key) <= m.get(key))) {
+        for (int i = 0; i < note.length; ++i) {
+            int count = n.containsKey(note[i]) ? n.get(note[i]) + 1 : 1;
+
+            if ( !(m.containsKey(note[i]) && count <= m.get(note[i])) ) {
                 System.out.println("No");
                 return;
             }
+
+            n.put(note[i], count);
         }
 
         System.out.println("Yes");
