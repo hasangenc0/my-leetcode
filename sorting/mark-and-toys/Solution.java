e7173e2a6acb517f270b80d3884a0a10ee02955d
hasan genc <hasangencx@hotmail.com>
2020-01-06 14:55:48 +0300

    Update Solution.java

--- sorting/mark-and-toys/Solution.java
+++ sorting/mark-and-toys/Solution.java
@@ -7,42 +7,124 @@ import java.util.concurrent.*;
 import java.util.regex.*;
 
 public class Solution {
+    static void merge(int arr[], int l, int m, int r) 
+{ 
+    int i, j, k; 
+    int n1 = m - l + 1; 
+    int n2 =  r - m; 
+  
+    /* create temp arrays */
+    int[] L = new int [n1];
+    int[] R = new int [n2]; 
+  
+    /* Copy data to temp arrays L[] and R[] */
+    for (i = 0; i < n1; i++) 
+        L[i] = arr[l + i]; 
+    for (j = 0; j < n2; j++) 
+        R[j] = arr[m + 1+ j]; 
+  
+    /* Merge the temp arrays back into arr[l..r]*/
+    i = 0; // Initial index of first subarray 
+    j = 0; // Initial index of second subarray 
+    k = l; // Initial index of merged subarray 
+    while (i < n1 && j < n2) 
+    { 
+        if (L[i] <= R[j]) 
+        { 
+            arr[k] = L[i]; 
+            i++; 
+        } 
+        else
+        { 
+            arr[k] = R[j]; 
+            j++; 
+        } 
+        k++; 
+    } 
+  
+    /* Copy the remaining elements of L[], if there 
+       are any */
+    while (i < n1) 
+    { 
+        arr[k] = L[i]; 
+        i++; 
+        k++; 
+    } 
+  
+    /* Copy the remaining elements of R[], if there 
+       are any */
+    while (j < n2) 
+    { 
+        arr[k] = R[j]; 
+        j++; 
+        k++; 
+    } 
+} 
+  
+/* l is for left index and r is right index of the 
+   sub-array of arr to be sorted */
+static void mergeSort(int arr[], int l, int r) 
+{ 
+    if (l < r) 
+    { 
+        // Same as (l+r)/2, but avoids overflow for 
+        // large l and h 
+        int m = l+(r-l)/2; 
+  
+        // Sort first and second halves 
+        mergeSort(arr, l, m); 
+        mergeSort(arr, m+1, r); 
+  
+        merge(arr, l, m, r); 
+    } 
+} 
+    // Complete the maximumToys function below.
+    static int maximumToys(int[] prices, int k) {
+        mergeSort(prices, 0, prices.length - 1); 
 
-    // Complete the countSwaps function below.
-    static void countSwaps(int[] a) {
-        int swaps = 0, len = a.length - 1;
-        for (int i = 0; i < len; ++i) {
-            for (int j = 0; j < len; ++j) {
-                if (a[j] > a[j + 1]) {
-                    int temp = a[j];
-                    a[j] = a[j + 1];
-                    a[j + 1] = temp;
-                    ++swaps;
-                }
+
+        int sum = 0, count = 0;
+        for (int i = 0; i < prices.length - 1; ++i) {
+            int total = sum + prices[i];
+            
+            if (total > k) {
+                break;
             }
+
+            sum = total;
+            ++count;
         }
-        System.out.println("Array is sorted in " +swaps+ " swaps.");
-        System.out.println("First Element: "+a[0]);
-        System.out.println("Last Element: "+a[len]);
+
+        return count;
     }
 
     private static final Scanner scanner = new Scanner(System.in);
 
-    public static void main(String[] args) {
-        int n = scanner.nextInt();
-        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
 
-        int[] a = new int[n];
+        String[] nk = scanner.nextLine().split(" ");
 
-        String[] aItems = scanner.nextLine().split(" ");
+        int n = Integer.parseInt(nk[0]);
+
+        int k = Integer.parseInt(nk[1]);
+
+        int[] prices = new int[n];
+
+        String[] pricesItems = scanner.nextLine().split(" ");
         scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
 
         for (int i = 0; i < n; i++) {
-            int aItem = Integer.parseInt(aItems[i]);
-            a[i] = aItem;
+            int pricesItem = Integer.parseInt(pricesItems[i]);
+            prices[i] = pricesItem;
         }
 
-        countSwaps(a);
+        int result = maximumToys(prices, k);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
 
         scanner.close();
     }
